15ad739c53d22ea89a1311c6d04bd930b631b126
amorimlt <julianeamorim.lt@gmail.com>
2022-03-25 21:31:57 -0300

    finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿const double gravidade = 9.80665;
+double velocidade, angulo, angulorad, altura, alcance;
+
+Console.Write("Entre com a velocidade, em m/s..: ");
+velocidade = Convert.ToDouble(Console.ReadLine()!);
+ 
+Console.Write("Entre com o ângulo, em graus....: ");
+angulo = Convert.ToDouble(Console.ReadLine()!);
+
+angulorad = angulo * (Math.PI / 180);
+
+altura = Math.Pow(velocidade * Math.Sin(angulorad ), 2) / (2 * gravidade);
+alcance = (Math.Pow(velocidade, 2) * Math.Sin(2 * angulorad)) / gravidade;
+
+Console.WriteLine($"Alcance........: {alcance:N2}");
+Console.WriteLine($"Altura máxima..: {altura:N2}");
+
+
